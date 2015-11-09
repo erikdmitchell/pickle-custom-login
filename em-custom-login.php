85f8c4c0971b97f7ab79f68fc930042214eb7596
erikdmitchell <erikdmitchell@gmail.com>
2015-11-09 11:16:23 -0500

    init of plugin
    v0.1.0

---
+++ em-custom-login.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: EM Custom Login
+Plugin URI:
+Description: Enables a completely customizable WordPress login, registration and password form.
+Version: 0.1.0
+Author: Erik Mitchell
+Author URI: http://erikmitchell.net
+*/
+
+
+?>
