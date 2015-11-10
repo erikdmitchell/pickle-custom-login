2f2a89c9fcdb648cd80782efb892547eee1a3c43
erikdmitchell <erikdmitchell@gmail.com>
2015-11-10 09:11:44 -0500

    finalized initial build out of error class

--- error.php
+++ error.php
@@ -1,25 +1,70 @@
 <?php
+/**
+ * EMCustomLoginErrors class.
+ *
+ * @sonce 0.10
+ */
 class EMCustomLoginErrors {
 
+	/**
+	 * __construct function.
+	 *
+	 * @access public
+	 * @return void
+	 */
 	public function __construct() {
 
 	}
 
+	/**
+	 * register_errors function.
+	 *
+	 * @access public
+	 * @return void
+	 */
 	public function register_errors() {
 		static $wp_error; // Will hold global variable safely
 		return isset($wp_error) ? $wp_error : ($wp_error = new WP_Error(null, null, null));
 	}
 
+	/**
+	 * show_error_messages function.
+	 *
+	 * Loop error codes and display errors
+	 *
+	 * @access public
+	 * @return void
+	 */
 	public function show_error_messages() {
 		if ($codes=$this->register_errors()->get_error_codes()) :
-			echo '<div class="custom-login-errors">';
-			  // Loop error codes and display errors
+			echo '<div class="custom-login-notice">';
 			  foreach ($codes as $code) :
 			  	$message = $this->register_errors()->get_error_message($code);
-			    echo '<span class="error"><strong>' . __('Error') . '</strong>: ' . $message . '</span><br/>';
+			    echo '<span class="error"><span class="title">'.__('Error').'</span>: '.$message.'</span><br/>';
 			  endforeach;
 			echo '</div>';
 		endif;
+	}
+
+	/**
+	 * format_error function.
+	 *
+	 * @access public
+	 * @param bool $message (default: false)
+	 * @param string $type (default: '')
+	 * @return void
+	 */
+	public function format_error($message=false,$type='') {
+		if (!$message)
+			return false;
+
+		$html=null;
+
+		$html.='<div class="custom-login-notice">';
+			$html.='<span class="'.$type.'">'.$message.'</span>';
+		$html.='</div>';
+
+		return $html;
 	}
 
 }
