82201771cfda9ea81a88c26f9254bb10b765fbbf
Erik <erikdmitchell@gmail.com>
2017-11-20 16:55:22 -0500

    added basic edit profile form to template

--- pcl-profile.php
+++ pcl-profile.php
@@ -27,7 +27,60 @@ class Pickle_Custom_Login_Profile {
     }
 
     public function update_user_profile() {
-        
+/*        
+ global $current_user, $wp_roles;
+get_currentuserinfo();
+
+
+$error = array();    
+// If profile was saved, update profile. 
+if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POST['action'] == 'update-user' ) {
+
+    // Update user password.
+    if ( !empty($_POST['pass1'] ) && !empty( $_POST['pass2'] ) ) {
+        if ( $_POST['pass1'] == $_POST['pass2'] )
+            wp_update_user( array( 'ID' => $current_user->ID, 'user_pass' => esc_attr( $_POST['pass1'] ) ) );
+        else
+            $error[] = __('The passwords you entered do not match.  Your password was not updated.', 'profile');
+    }
+
+    // Update user information.
+    if ( !empty( $_POST['url'] ) )
+       wp_update_user( array ('ID' => $current_user->ID, 'user_url' => esc_attr( $_POST['url'] )));
+    if ( !empty( $_POST['email'] ) ){
+        if (!is_email(esc_attr( $_POST['email'] )))
+            $error[] = __('The Email you entered is not valid.  please try again.', 'profile');
+        elseif(email_exists(esc_attr( $_POST['email'] )) != $current_user->id )
+            $error[] = __('This email is already used by another user.  try a different one.', 'profile');
+        else{
+            wp_update_user( array ('ID' => $current_user->ID, 'user_email' => esc_attr( $_POST['email'] )));
+        }
+    }
+
+    if ( !empty( $_POST['first-name'] ) )
+        update_user_meta( $current_user->ID, 'first_name', esc_attr( $_POST['first-name'] ) );
+    if ( !empty( $_POST['last-name'] ) )
+        update_user_meta($current_user->ID, 'last_name', esc_attr( $_POST['last-name'] ) );
+    if ( !empty( $_POST['display_name'] ) )
+        wp_update_user(array('ID' => $current_user->ID, 'display_name' => esc_attr( $_POST['display_name'] )));
+      update_user_meta($current_user->ID, 'display_name' , esc_attr( $_POST['display_name'] ));
+    if ( !empty( $_POST['description'] ) )
+        update_user_meta( $current_user->ID, 'description', esc_attr( $_POST['description'] ) );
+
+    // Redirect so the page will show updated info.
+  // I am not Author of this Code- i dont know why but it worked for me after changing below line to if ( count($error) == 0 ){ 
+    if ( count($error) == 0 ) {
+        //action hook for plugins and extra fields saving
+        do_action('edit_user_profile_update', $current_user->ID);
+        wp_redirect( get_permalink().'?updated=true' ); exit;
+    }       
+}
+
+
+            <?php if ( $_GET['updated'] == 'true' ) : ?> <div id="message" class="updated"><p>Your profile has been updated.</p></div> <?php endif; ?>
+                <?php if ( count($error) > 0 ) echo '<p class="error">' . implode("<br />", $error) . '</p>'; ?>
+*/
+       
     }
 
 }
--- templates/profile.php
+++ templates/profile.php
@@ -1,10 +1,101 @@
 <div class="pcl-profile">
-	<form id="pcl-profile-form" class="pcl-profile-form" action="" method="post">
-		<fieldset>
-			
-			
-			
-		</fieldset>
-	</form>
+
+    <?php if (!is_user_logged_in()) : ?>
+
+        <p class="warning">
+            <?php _e('You must be logged in to edit your profile.', 'pcl'); ?>
+        </p><!-- .warning -->
+            
+    <?php else : ?>
+        
+        <?php $current_user = wp_get_current_user(); ?>
+            
+        <h3>Update Information for <?php echo $current_user->user_login ?></h3>
+        
+        <form method="post" id="adduser" class="pcl-profile-form" action="" method="post">
+                       
+            <p class="form-username">
+                <label for="first-name"><?php _e('First Name', 'profile'); ?></label>
+                <input class="text-input" name="first-name" type="text" id="first-name" value="<?php the_author_meta( 'first_name', $current_user->ID ); ?>" />
+            </p>
+            
+            <p class="form-username">
+                <label for="last-name"><?php _e('Last Name', 'profile'); ?></label>
+                <input class="text-input" name="last-name" type="text" id="last-name" value="<?php the_author_meta( 'last_name', $current_user->ID ); ?>" />
+            </p>
+            
+            <p class="form-display_name">
+                <label for="display_name"><?php _e('Display name publicly as') ?></label>
+    	
+        		<select name="display_name" id="display_name">
+        		    
+        		    <?php
+                    $public_display = array();
+        			$public_display['display_nickname']  = $current_user->nickname;
+        			$public_display['display_username']  = $current_user->user_login;
+        
+        			if ( !empty($current_user->first_name) )
+        				$public_display['display_firstname'] = $current_user->first_name;
+        
+        			if ( !empty($current_user->last_name) )
+        				$public_display['display_lastname'] = $current_user->last_name;
+        
+        			if ( !empty($current_user->first_name) && !empty($current_user->last_name) ) {
+        				$public_display['display_firstlast'] = $current_user->first_name . ' ' . $current_user->last_name;
+        				$public_display['display_lastfirst'] = $current_user->last_name . ' ' . $current_user->first_name;
+        			}
+        
+        			if ( ! in_array( $current_user->display_name, $public_display ) ) // Only add this if it isn't duplicated elsewhere
+        				$public_display = array( 'display_displayname' => $current_user->display_name ) + $public_display;
+        
+        			$public_display = array_map( 'trim', $public_display );
+        			$public_display = array_unique( $public_display );
+        
+        			foreach ( $public_display as $id => $item ) : ?>
+            			<option <?php selected( $current_user->display_name, $item ); ?>><?php echo $item; ?></option>
+                    <?php endforeach; ?>
+                    
+        		</select>
+    		
+    		</p>
+                        
+            <p class="form-email">
+                <label for="email"><?php _e('E-mail *', 'profile'); ?></label>
+                <input class="text-input" name="email" type="text" id="email" value="<?php the_author_meta( 'user_email', $current_user->ID ); ?>" />
+            </p>
+            
+            <p class="form-url">
+                <label for="url"><?php _e('Website', 'profile'); ?></label>
+                <input class="text-input" name="url" type="text" id="url" value="<?php the_author_meta( 'user_url', $current_user->ID ); ?>" />
+            </p>
+            
+            <p class="form-password">
+                <label for="pass1"><?php _e('Password *', 'profile'); ?> </label>
+                <input class="text-input" name="pass1" type="password" id="pass1" />
+            </p>
+            
+            <p class="form-password">
+                <label for="pass2"><?php _e('Repeat Password *', 'profile'); ?></label>
+                <input class="text-input" name="pass2" type="password" id="pass2" />
+            </p>
+            
+            <p class="form-textarea">
+                <label for="description"><?php _e('Biographical Information', 'profile') ?></label>
+                <textarea name="description" id="description" rows="3" cols="50"><?php the_author_meta( 'description', $current_user->ID ); ?></textarea>
+            </p>
+    
+            <?php do_action('edit_user_profile', $current_user); ?>
+
+            <p class="form-submit">                
+                <input name="updateuser" type="submit" id="updateuser" class="submit button" value="<?php _e('Update', 'profile'); ?>" />
+                
+                <?php wp_nonce_field( 'update-user_'. $current_user->ID ) ?>
+                
+                <input name="action" type="hidden" id="action" value="update-user" />
+            </p>
+        
+        </form>
+        
+    <?php endif; ?>
 
 </div>
