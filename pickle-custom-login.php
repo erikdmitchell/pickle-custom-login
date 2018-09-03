aed00e2852a19a4796b56b61d0093dacc2ae421d
Erik <erikdmitchell@gmail.com>
2018-09-03 14:35:27 -0400

    removed github updater

--- pickle-custom-login.php
+++ pickle-custom-login.php
@@ -83,7 +83,6 @@ final class PickleCustomLogin {
 		include_once(PCL_PATH.'pcl-user-activation.php');
 		include_once(PCL_PATH.'pcl-email.php');
 		include_once(PCL_PATH.'libraries/recaptchalib.php'); // google recaptcha library
-		include_once(PCL_PATH.'updater/updater.php'); // git hub update
 		
 		new Pickle_Custom_Login();
 		new Pickle_Custom_Login_Reset_Password();
@@ -106,34 +105,7 @@ final class PickleCustomLogin {
 		$this->errors=new Pickle_Custom_Login_Errors();
 		$this->email=new Pickle_Custom_Login_Email();
 		$this->pages=get_option('pcl_pages');
-	}
-	
-	public function plugin_updater() {
-		if (!is_admin())
-			return false;
-	
-		if (!defined('WP_GITHUB_FORCE_UPDATE'))
-			define('WP_GITHUB_FORCE_UPDATE', true);
-			
-		$username='erikdmitchell';
-		$repo_name='pickle-custom-login';
-		$folder_name='pickle-custom-login';
-	    
-	    $config = array(
-	        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-	        'proper_folder_name' => $folder_name, // this is the name of the folder your plugin lives in
-	        'api_url' => 'https://api.github.com/repos/'.$username.'/'.$repo_name, // the github API url of your github repo
-	        'raw_url' => 'https://raw.github.com/'.$username.'/'.$repo_name.'/master', // the github raw url of your github repo
-	        'github_url' => 'https://github.com/'.$username.'/'.$repo_name, // the github url of your github repo
-	        'zip_url' => 'https://github.com/'.$username.'/'.$repo_name.'/zipball/master', // the zip url of the github repo
-	        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-	        'requires' => '4.0', // which version of WordPress does your plugin require?
-	        'tested' => '4.9', // which version of WordPress is your plugin tested up to?
-	        'readme' => 'readme.txt', // which file to use as the readme for the version number
-	    );
-	   
-		new WP_GitHub_Updater($config);
-	}	
+	}s	
 
 }
 
