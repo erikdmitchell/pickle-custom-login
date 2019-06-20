9d513ae8989051e60ab4142a49eadef2e3876edd
Erik <erikdmitchell@gmail.com>
2019-06-20 15:48:46 -0400

    updated to v1.1.1

--- pickle-custom-login.php
+++ pickle-custom-login.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pickle Custom Login
  * Plugin URI:
  * Description: Enables a completely customizable WordPress login, registration and password form.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Erik Mitchell
  * Author URI: http://erikmitchell.net
  * License: GPL-2.0+
